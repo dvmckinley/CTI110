3c90f6ad2594f5bde53c7e5d9197abe24c0f07af
dvmckinley <55704084+dvmckinley@users.noreply.github.com>
2019-09-23 10:50:45 -0400

    Add files via upload
    First Upload

---
+++ P2T1_SalesPrediction_DavidMcKinley.py
@@ -0,0 +1,13 @@
+
+# A brief description of the project
+# Date
+# CTI-110 P2T1 - Sales Prediction
+# David McKinley
+# Get the projected sales total
+
+total_sales=float(input('Enter the projected sales,:'))
+profit=total_sales*0.23
+print('the profit is $',format( profit,',.2f'))
+
+                                
+
